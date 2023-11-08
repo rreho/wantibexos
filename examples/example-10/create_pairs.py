b4dccda7bb1b56a9ca0662a9cf305009b4d5c417
Pedro Melo <pmelo1089@gmail.com>
2023-11-08 09:53:04 +0100

    added file to create nnkpts block. added new mmn file with overlaps

---
+++ examples/example-10/create_pairs.py
@@ -0,0 +1,12 @@
+s1 = 'begin nnkpts\n'
+
+s2 = 'end nnkpts\n'
+
+
+# we canot create only half of the k-point pairs, since the in the nnkpts block all kpoints must have the same number of neighbours
+with open('nnkpts_list.dat','w') as f:
+  f.write(s1)
+  for i in range(1,28):
+    for j in range(1,28):
+      f.write("%i %i 0 0 0\n" %(i,j))
+  f.write(s2)
